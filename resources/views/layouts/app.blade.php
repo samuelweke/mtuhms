650d069561582e8a00e1f7b5676ce9ac472ac994
wekesamuel <wekesamuel@yahoo.com>
2019-06-13 03:21:22 +0100

    First commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>MTUHMS @yield('title')</title>
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    
+    
+    <style>
+        body{
+        overflow-x: hidden;
+        background: white;
+        }
+    </style>
+    
+
+</head>
+    <div id="app">
+            <main class="py-0">
+                @yield('content')
+            </main>
+       
+    </body>
+
+    
+
+
+</html>
---
+++ resources/views/signin.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+<link rel="stylesheet" href="{{ asset('css/signin.css') }}">
+
+@section('content')
+    
+    <div class="main-content">
+		<div class="login-area">    
+			<div class="login">
+				<div class="header">
+					<h1> MTU Hospital <br> Management System </h1>
+				</div>
+				<form>                    
+	                <div class="form-horizontal">
+	                    <div class="form-group">
+	                      <input type="email" class="form-control" placeholder="Matric No" required />
+	                    </div>
+	                    <div class="form-group">
+						   <div>
+						      	<input type="password" name="password"  class="form-control" placeholder="Password" required />
+						    </div>
+						</div>
+	                  </div>
+                    <button type="submit" class="btn btn-green btn-block">Login</button>
+                    <div class="login-footer">
+						<a href=""><p>Forgot your password?</p></a>
+						<a href=""><p>Don't have an account?</p></a>
+					</div>
+				</form>
+			</div>
+		</div>
+		<div class="image-area">
+		</div>
+	</div>
+
+
+@endsection
